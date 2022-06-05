0ee545c61f2d587160a25c97107f8a65597200e0
Johannes Fischer - Merkur <joh.fischer@outlook.com>
2022-06-05 12:29:03 +0200

    integrate aggregator in logger

--- logger/logger.py
+++ logger/logger.py
@@ -4,7 +4,20 @@ import csv
 import os
 import torch
 
-from .aggregator import Aggregator
+
+class Aggregator:
+    def __init__(self):
+        """
+        Aggregate sum and average.
+        """
+        self.sum = 0.
+        self.avg = 0.
+        self.count = 0
+
+    def update(self, val: float, n: int = 1):
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 class Logger:
